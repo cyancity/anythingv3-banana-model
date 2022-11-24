912940284d3bd1c5a3e69fd126962816e926b349
Symphony <lichengyang@aliyun.com>
2022-11-24 18:36:27 +0800

    init anything model

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from transformers import pipeline
+from diffusers import StableDiffusionPipeline
 import torch
+
+model_id = "Linaqruf/anything-v3.0"
+
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+
+    # device = 0 if torch.cuda.is_available() else -1
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    model = pipe.to("cuda")
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -18,9 +22,9 @@ def inference(model_inputs:dict) -> dict:
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
-    
+
     # Run the model
     result = model(prompt)
 
     # Return the results as a dictionary
-    return result
+    return dict((x, y) for x, y in result)
